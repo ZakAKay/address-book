67465bcff3651c870f1e59afb3f31bcaabc12ed0
ZakAKay <zakkay@ymail.com>
2022-08-07 17:09:29 -0500

    final version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-function App() {
-  return 
-  <div>
+import React from "react";
+import AddressBook from "./Components/AddressBook";
 
-  </div>;
+export default function App() {
+  return (
+    <div>
+      <AddressBook />
+    </div>
+  );
 }
-
-export default App;
--- src/Components/AddressBook.jsx
+++ src/Components/AddressBook.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Details from "./Details";
+export default function AddressBook(props) {
+  const [people, setPeople] = useState({ results: [] });
 
-export default function AddressBook() {
+  useEffect(() => {
+    const fetchPeople = () => {
+      fetch("https://randomuser.me/api?results=25")
+        .then((response) => {
+          return response.json();
+        })
+        .then((people) => {
+          setPeople(people);
+          console.log(people);
+        });
+    };
+    fetchPeople();
+  }, []);
+
   return (
-    <div>AddressBook
-        
+    <div>
+      <ol>
+        {people.results.map((person, index) => {
+          return (
+            <li key={index}>
+              <h2>
+                {person.name.title.toUpperCase()}{" "}
+                {person.name.first.toUpperCase()}{" "}
+                {person.name.last.toUpperCase()}
+              </h2>
+              <p>Gender: {person.gender}</p>
+              <p>
+                Location: {person.location.street.name}{" "}
+                {person.location.street.number} {person.location.city}{" "}
+                {person.location.state} {person.location.country}
+              </p>
+              <img src={`${person.picture.thumbnail}`}></img> <br></br>
+              <Details person={person} />
+            </li>
+          );
+        })}
+      </ol>
     </div>
-  )
+  );
 }
